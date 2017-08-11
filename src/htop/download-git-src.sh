b4a06df1d61bdce170084132b995f4d7c96a66c4
gnsyxiang <gnsyxiang@163.com>
2017-08-11 01:36:09 -0700

    feat: porting htop to x1800
    porting htop to x1800

---
+++ src/htop/download-git-src.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "--------------------"
+echo "download the ncurses"
+echo "--------------------"
+git clone https://github.com/mirror/ncurses.git
+
+
+echo "--------------------"
+echo "download the htop"
+echo "--------------------"
+git clone https://github.com/hishamhm/htop.git
+
